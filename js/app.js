fe5f6f8692619b62ca2b3de00b662d1b5280fe79
HellWolhardZeli <aditya11maharana@gmail.com>
2019-09-12 16:33:28 +0530

    mobile swipe done

--- js/app.js
+++ js/app.js
@@ -6,19 +6,9 @@
 // }
 
 
-function elementYPosition(eID) {
-    var elm = document.getElementById(eID);
-    var y = elm.offsetTop;
-    var node = elm;
-    while (node.offsetParent && node.offsetParent != document.body) {
-        node = node.offsetParent;
-        y += node.offsetTop;
-    }
-    return y;
-}
+//scrollTo of javaScript but with custom animations
 
-
-
+var mobile = false;
 
 function smoothScroll(stopY) {
     var startY = window.pageYOffset || document.documentElement.scrollTop;
@@ -29,7 +19,7 @@ function smoothScroll(stopY) {
         scrollTo(0, stopY);
         return;
     }
-    var speed = Math.round(distance / 20);
+    var speed = Math.round(distance / 100);
     if (speed >= 20) speed = 20;
     var step = Math.round(distance / 25);
     var leapY = stopY > startY ? startY + step : startY - step;
@@ -53,6 +43,66 @@ function smoothScroll(stopY) {
 
 
 
+//
+//
+//
+//
+//
+//
+
+document.querySelector("#swipezone").addEventListener("touchstart", startTouch, false);
+document.querySelector("#swipezone").addEventListener("touchmove", moveTouch, false);
+
+// Swipe Up / Down / Left / Right
+var initialX = null;
+var initialY = null;
+
+function startTouch(e) {
+    initialX = e.touches[0].clientX;
+    initialY = e.touches[0].clientY;
+    mobile = true;
+};
+
+function moveTouch(e) {
+    if (initialX === null) {
+        return;
+    }
+
+    if (initialY === null) {
+        return;
+    }
+
+    var currentX = e.touches[0].clientX;
+    var currentY = e.touches[0].clientY;
+
+    var diffX = initialX - currentX;
+    var diffY = initialY - currentY;
+
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+        // sliding horizontally
+        if (diffX > 0) {
+            // swdocument.querySelector("#swipezone") 
+            console.log("swiped left");
+        } else {
+            // swiped right
+            console.log("swiped right");
+        }
+    } else {
+        // sliding vertically
+        if (diffY > 0) {
+            // swiped up
+            console.log("swiped up");
+        } else {
+            // swiped down
+            console.log("swiped down");
+        }
+    }
+
+    initialX = null;
+    initialY = null;
+
+    e.preventDefault();
+};
 
 
 
@@ -88,11 +138,11 @@ var snapBtm = 1 - snapTop;
 
 
 
-window.onwheel = function (ev) {
+window.onscroll = function (ev) {
     clearTimeout(timeout);
     timeout = setTimeout(function () {
         snapScroll();
-    }, 30);
+    }, 60);
 };
 
 function snapScroll() {
